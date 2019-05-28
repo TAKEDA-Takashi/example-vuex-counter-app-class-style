fcd21283448502cba7b85ca28e0100db015e096d
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2019-05-28 14:17:40 +0900

    Counterコンポーネントを実装

---
+++ src/views/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    Clicked: {{ count }} times
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">Increment if odd</button>
+    <button @click="incrementAsync">Increment async</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { vxm } from '@/store';
+
+@Component
+export default class Counter extends Vue {
+
+  get count() {
+    return vxm.counter.count;
+  }
+
+  private increment() {
+    vxm.counter.increment();
+  }
+
+  private decrement() {
+    vxm.counter.decrement();
+  }
+
+  private incrementIfOdd() {
+    vxm.counter.incrementIfOdd();
+  }
+
+  private incrementAsync() {
+    vxm.counter.incrementAsync();
+  }
+}
+</script>
