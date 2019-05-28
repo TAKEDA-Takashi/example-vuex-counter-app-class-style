2765b49568feb599ea40e3b785f99422e394370b
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2019-05-28 14:18:08 +0900

    Counterコンポーネントへのルーティングを追加

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Counter from './views/Counter.vue';
 
 Vue.use(Router);
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/counter',
+      name: 'counter',
+      component: Counter,
     },
     {
       path: '/about',
